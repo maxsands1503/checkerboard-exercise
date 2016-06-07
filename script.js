0a12fd1164173342f336028bdcd4e414db5d41b3
Maxwell Sands <maxwellsands@Maxwells-MacBook-Air.local>
2016-06-07 10:18:49 -0600

    Final for flashing

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-// // Your JS goes here
-//
-// for(var count = 0; count < 80; count++){
-//
-//   if(count === 0){
-//       document.write('<br/><div style="background-color:#000000;float:left; width: 11.1%; padding:11.1%; display:inline-block;"></div>');
-//   }
-//   if(count === 1){
-//     document.write('<br/><div style="background-color:#FF0000;float:left; width: 11.1%; padding:11.1%; display:inline-block;"></div>');
-//   }
-//   else  if (count % 2 === 0){
-//       document.write('<br/><div style="background-color:#FF0000;float:left; width: 11.1%; padding:11.1%; display:inline-block;"></div>');
-//         }
-//
-//         else if(count % 5 === 0 && count % 2 !== 0){
-//           document.write('<br/><div style="background-color:#000000; float:left; width: 11.1%; padding:11.1%; display:inline-block;"></div>');
-//           }
-//           else if(count % 5 === 0 && count % 2 === 0){
-//         document.write('<br/><div style="background-color:#FF0000; float:left; width: 11.1%; padding:11.1%; display:inline-block;"></div>');
-//           }
-//           else if (count % 3 === 0 && count %2 !== 0) {
-//             document.write('<br/><div style="background-color:#FF0000; float:left; width: 11.1%; padding:11.1%; display:inline-block;"></div>');
-//           }
-//     }
 var container = document.createElement("div");
 container.style.width = "100vw";
 container.style.height = "100vh"
@@ -31,27 +7,22 @@ container.style.padding = "0";
 document.body.appendChild(container);
 
 
-
 for (var counter = 0; counter < 63; counter++) {
-
-  if(counter % 2 === 0){
     var div1 = document.createElement("div");
-    div1.style.background = '#'+'0123456789abcdef'.split('').map(function(v,i,a){
-  return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
+    div1.style.background = getRando();
     div1.style.width = "11.1%";
     div1.style.paddingBottom = "11.1%";
     div1.style.float = "left";
     div1.style.display = "inline-block";
     container.appendChild(div1);
   }
-  else{
-    var div2 = document.createElement("div");
-    div2.style.background = '#'+'0123456789abcdef'.split('').map(function(v,i,a){
-  return i>5 ? null : a[Math.floor(Math.random()*16)] }).join('');
-    div2.style.width = "11.1%";
-    div2.style.paddingBottom = "11.1%";
-    div2.style.float = "left";
-    div2.style.display = "inline-block";
-    container.appendChild(div2);
+
+  function getRando() {
+  var color = '#' + '0123456789abcdef'.split('').map(function(x,y, z){
+    return z[Math.floor(Math.random()*16)] }).join('').slice(0,6);
+  return color
   }
-}
+
+ setTimeout(function(){
+    window.location.reload(1);
+ }, 2000);
